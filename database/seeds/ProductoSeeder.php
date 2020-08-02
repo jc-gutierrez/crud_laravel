8f587f69b447a042240831820e171d31cbf5c51e
jc-gutierrez <jcgq21@gmail.com>
2020-08-02 09:51:25 -0400

    close #8

--- database/seeds/ProductoSeeder.php
+++ database/seeds/ProductoSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Producto;
 
 class ProductoSeeder extends Seeder
 {
@@ -11,6 +12,21 @@ class ProductoSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $productos = [
+            [
+                'nombre' => 'Calzados de Varon',
+                'cantidad' => 100,
+                'precio' => 215,
+                'descripcion' => 'Calzados para varones, niños y adultos'
+            ], [
+                'nombre' => 'Calzados de Mujer',
+                'cantidad' => 200,
+                'precio' => 300,
+                'descripcion' => 'Calzados para damas, niñas y adultos'
+            ]
+        ];
+        foreach ($productos as $producto) {
+            Producto::firstOrCreate($producto);
+        }
     }
 }
